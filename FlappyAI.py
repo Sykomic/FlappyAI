c5e880d7a56a5a9953d744e46e295b6bc53b5bf6
Daesik Choi <seafood0917@gmail.com>
2019-12-11 18:31:29 -0800

    loading images

--- FlappyAI.py
+++ FlappyAI.py
@@ -7,9 +7,8 @@ import random
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
-BIRD_IMGS = [pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird1.png'))),
+BIRD_IMGS = [
+        pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird1.png'))),
         pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird2.png'))),
         pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird3.png')))
         ]
-
-print(BIRD_IMGS)
