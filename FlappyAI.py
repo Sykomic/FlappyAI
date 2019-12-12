23f18925e22805c577b3f3d5c8ff890f8d51e393
Daesik Choi <seafood0917@gmail.com>
2019-12-11 18:28:40 -0800

    loading images

---
+++ FlappyAI.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+
+BIRD_IMGS = [pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird1.png'))),
+        pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird2.png'))),
+        pg.transform.scale2x(pg.image.load(os.path.join('imgs', 'bird3.png')))
+        ]
+
+print(BIRD_IMGS)
