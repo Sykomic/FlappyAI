a6a02f8eaec2608a753d250dbcafb846f510d676
Daesik Choi <seafood0917@gmail.com>
2019-12-26 09:26:52 +0900

    Finishing the Graphics

--- FlappyAI.py
+++ FlappyAI.py
@@ -114,31 +114,86 @@ class Pipe:
         bird_mask = bird.get_mask()
         top_mask = pg.mask.from_surface(self.PIPE_TOP)
         bottom_mask = pg.mask.from_surface(self.PIPE_BOTTOM)
-        top_offset = (self.x - bird.x)
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
 
-        # Pixel Perfect Collision 11:30
+        # Checking if bird and pipe collide, otherwise it returns None
+        t_point = bird_mask.overlap(top_mask, top_offset)
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+
+        if t_point or b_point:
+            return True
+        return False
 
 
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
 
-def draw_window(win, bird):
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self, win):
+        win.blit(self.IMG, (self.x1, self.y))
+        win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, bird, pipes, base):
     win.blit(BG_IMG, (0, 0))
+    for pipe in pipes:
+        pipe.draw(win)
+    base.draw(win)
     bird.draw(win)
     pg.display.update()
 
 
 def main():
-    bird = Bird(200, 200)
-    run = True
+    bird = Bird(230, 350)
+    base = Base(730)
+    pipes = [Pipe(600)]
     win = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pg.time.Clock()
+    score = 0
 
+    run = True
     while run:
         clock.tick(30)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
         #bird.move()
-        draw_window(win, bird)
+        add_pipe = False
+        rem = []  # removed pipes
+        for pipe in pipes:
+            if pipe.collide(bird, win):
+                pass
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+            pipe.move()
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(600))
+
+        for r in rem:
+            pipes.remove(r)
+        base.move()
+        draw_window(win, bird, pipes, base)
     pg.quit()
     quit()
 
